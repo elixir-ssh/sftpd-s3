100a10f28b816f117c90a43e5f6cdc6db1c9f8c6
Michael J. Cohen <mjc@kernel.org>
2022-06-22 18:02:24 -0600

    Operations tests

--- test/sftpd_s3/s3/operations_test.exs
+++ test/sftpd_s3/s3/operations_test.exs
@@ -5,17 +5,68 @@ defmodule SftpdS3.S3.OperationsTest do
 
   alias SftpdS3.S3.Operations
 
-  setup do
-    ExAws.S3.put_bucket("operations-test-bucket", "us-east-1")
-    |> ExAws.request!()
+  describe "Directory operations" do
+    setup do
+      bucket = "directory-operations"
 
-    :ok
+      ExAws.S3.put_bucket(bucket, "us-east-1")
+      |> ExAws.request!()
+
+      on_exit(fn ->
+        bucket
+        |> ExAws.S3.list_objects()
+        |> ExAws.stream!()
+        |> Enum.each(&ExAws.request!(ExAws.S3.delete_object(bucket, &1.key)))
+
+        ExAws.S3.delete_bucket(bucket) |> ExAws.request()
+      end)
+
+      %{bucket: bucket}
+    end
+
+    test "list_dir", %{bucket: bucket} do
+      assert ['.', '..'] = Operations.list_dir("/", bucket)
+    end
+
+    test "make_dir", %{bucket: bucket} do
+      assert ['.', '..'] = Operations.list_dir('/', bucket)
+      assert :ok = Operations.make_dir('/make_dir', bucket)
+      assert ['make_dir', '.', '..'] = Operations.list_dir('/', bucket)
+    end
+
+    test "read_link_info /", %{bucket: bucket} do
+      assert {:ok,
+              {:file_info, 640, :directory, :read, time, time, time, 16877, 20, 16_777_230, 0, 2,
+               1, 1}} = Operations.read_link_info('/', bucket)
+    end
   end
 
-  test "list empty dir" do
-    ExAws.S3.put_bucket("list-empty-dir", "us-east-1")
-    |> ExAws.request!()
+  describe "File operations" do
+    setup do
+      bucket = "file-operations"
 
-    assert true
+      ExAws.S3.put_bucket(bucket, "us-east-1")
+      |> ExAws.request!()
+
+      on_exit(fn ->
+        bucket
+        |> ExAws.S3.list_objects()
+        |> ExAws.stream!()
+        |> Enum.each(&ExAws.request!(ExAws.S3.delete_object(bucket, &1.key)))
+
+        ExAws.S3.delete_bucket(bucket) |> ExAws.request()
+      end)
+
+      %{bucket: bucket}
+    end
+
+    test "read_link_info /file", %{bucket: bucket} do
+      ExAws.S3.put_object(bucket, "file", "some text") |> ExAws.request!()
+
+      # TODO: size should be 9
+      assert {:ok,
+              {:file_info, 0, :regular, :read_write, time, time, time, 33261, 1, 0, 0, _, 1, 1}} =
+               Operations.read_link_info('/file', bucket)
+    end
   end
 end
