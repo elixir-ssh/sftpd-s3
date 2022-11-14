ddd72cf30693fb335051f0280421d4d232ffb7fc
Michael J. Cohen <mjc@kernel.org>
2022-11-14 12:44:47 -0700

    Add rudimentary start/stop

--- lib/sftpd_s3.ex
+++ lib/sftpd_s3.ex
@@ -4,15 +4,48 @@ defmodule SftpdS3 do
   """
 
   @doc """
-  Hello world.
+  Start a new test SFTP server.
+
+  This should be replaced with a child_spec.
 
   ## Examples
 
-      iex> SftpdS3.hello()
-      :world
-
+      iex> {:ok, pid} = SftpdS3.start_server(65_530)
   """
-  def hello do
-    :world
+
+  def start_server(port \\ 22)
+
+  @spec start_server(non_neg_integer) :: any
+  def start_server(port) do
+    :ssh.daemon(port, [
+      {:max_sessions, 1},
+      {:user_passwords, [{'user', 'password'}]},
+      {:system_dir, 'test/fixtures/ssh_keys'},
+      {:subsystems,
+       [
+         :ssh_sftpd.subsystem_spec(
+           cwd: '/',
+           root: '/',
+           file_handler: {__MODULE__, %{}}
+         )
+       ]}
+    ])
   end
+
+
+  @doc """
+    Stop a SSH daemon.
+
+    This stops the whole daemon.
+    You probably want to roll your own thing that removes the sftp handler instead.
+
+    ## Examples
+
+        iex> {:ok, pid} = SftpdS3.start_server(65_529)
+        iex> SftpdS3.stop_server(pid)
+  """
+  def stop_server(pid) do
+    :ssh.stop_daemon(pid)
+  end
+
 end
